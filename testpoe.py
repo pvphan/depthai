cf58557197b38381fcb19a5c53ffdc33f4a0ddf7
Vinh Phan <pvphan90@gmail.com>
2022-05-05 19:51:51 -0700

    just rgb for now

--- testpoe.py
+++ testpoe.py
@@ -1,22 +1,33 @@
 import cv2
 import depthai as dai
+import numpy as np
 
-pipeline = dai.Pipeline()
 
-camRgb = pipeline.createColorCamera()
+def main():
+    pipeline = dai.Pipeline()
 
-xoutRgb = pipeline.createXLinkOut()
-xoutRgb.setStreamName("rgb")
-camRgb.preview.link(xoutRgb.input)
+    camRgb = pipeline.createColorCamera()
 
-device_info = dai.DeviceInfo()
-device_info.state = dai.XLinkDeviceState.X_LINK_BOOTLOADER
-device_info.desc.protocol = dai.XLinkProtocol.X_LINK_TCP_IP
-device_info.desc.name = "192.168.0.225"
+    xoutRgb = pipeline.createXLinkOut()
+    xoutRgb.setStreamName("rgb")
+    camRgb.preview.link(xoutRgb.input)
 
-with dai.Device(pipeline, device_info) as device:
-    qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
-    while True:
-        cv2.imshow("rgb", qRgb.get().getCvFrame())
-        if cv2.waitKey(1) == ord('q'):
-            break
+    device_info = dai.DeviceInfo()
+    device_info.state = dai.XLinkDeviceState.X_LINK_BOOTLOADER
+    device_info.desc.protocol = dai.XLinkProtocol.X_LINK_TCP_IP
+    device_info.desc.name = "192.168.0.225"
+
+    with dai.Device(pipeline, device_info) as device:
+        qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+        #qLeft = device.getOutputQueue(name="left", maxSize=4, blocking=False)
+        #qRight = device.getOutputQueue(name="right", maxSize=4, blocking=False)
+        while True:
+            cv2.imshow("rgb", qRgb.get().getCvFrame())
+            #leftRight = np.hstack((qLeft.get().getCvFrame(), qRight.get().getCvFrame()))
+            #cv2.imshow("left-right", leftRight)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+
+if __name__ == "__main__":
+    main()
